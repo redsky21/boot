959f100db380a551912fb65b49de29720518da64
sweet xoju <sweetxoju@gmail.com>
2021-12-31 10:33:48 +0900

    Initial commit

---
+++ src/main/java/com/chs/boot/BootApplication.java
@@ -0,0 +1,20 @@
+package com.chs.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello AWS";
+    }
+
+}
