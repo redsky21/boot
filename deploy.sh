f54fcd66a9ec2cb755351f0268931cd063497314
sweet xoju <sweetxoju@gmail.com>
2021-12-31 18:59:37 +0900

    12313254

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/opt/testapp
 cd $REPOSITORY
 
-APP_NAME=action_codedeploy
+APP_NAME=boot
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
