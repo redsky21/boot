3286124a90d7449669f2fbc0076ccaa31ba1c3e5
sweetxoju <sweetxoju@gmail.com>
2022-01-06 15:55:10 +0900

    add: cors setting

--- src/main/java/com/chs/boot/BootApplication.java
+++ src/main/java/com/chs/boot/BootApplication.java
@@ -2,11 +2,14 @@ package com.chs.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+
 public class BootApplication {
 
     public static void main(String[] args) {
@@ -15,6 +18,12 @@ public class BootApplication {
     @GetMapping("/hello")
     public String hello(){
         return "goodstart";
+    }
+
+    @PostMapping("/hello2")
+//    @CrossOrigin("*")
+    public String hello2(){
+        return "goodstart2";
     }
 
 }
