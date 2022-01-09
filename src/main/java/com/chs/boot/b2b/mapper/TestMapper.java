16693273cc1c2bd9ddc58d2375ed42e8b25590b9
pig1 <redsky21@lgcns.com>
2022-01-09 14:34:27 +0900

    add: Multi Datasource

---
+++ src/main/java/com/chs/boot/b2b/mapper/TestMapper.java
@@ -0,0 +1,12 @@
+package com.chs.boot.b2b.mapper;
+
+import com.chs.boot.pub.model.PubTemplateDO;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TestMapper {
+
+    String getDb2Dual() throws Exception;
+
+}
