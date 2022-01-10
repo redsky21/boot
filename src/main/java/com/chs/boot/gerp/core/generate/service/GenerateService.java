d7a682155f233b50bb4214df7b0891c5d878e6ba
pig1 <redsky21@lgcns.com>
2022-01-10 09:29:03 +0900

    add: gen source23

--- src/main/java/com/chs/boot/gerp/core/generate/service/GenerateService.java
+++ src/main/java/com/chs/boot/gerp/core/generate/service/GenerateService.java
@@ -1,8 +1,22 @@
 package com.chs.boot.gerp.core.generate.service;
 
+import com.chs.boot.gerp.b2b.generate.mapper.B2bGenerateMapper;
+import com.chs.boot.gerp.b2b.generate.model.SchemaColumnConditionVO;
+import com.chs.boot.gerp.b2b.generate.model.SchemaColumnVO;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GenerateService {
 
+    @Autowired
+    B2bGenerateMapper b2bGenerateMapper;
+
+    public List<SchemaColumnVO> retrieveColumnSchema(
+        SchemaColumnConditionVO schemaColumnConditionVO, String dataSetName) {
+        List<SchemaColumnVO> result = b2bGenerateMapper.retrieveColumnSchema(
+            schemaColumnConditionVO);
+        return result;
+    }
 }
