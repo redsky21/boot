1261d407311a63b2fdf34c461b67d0921acf9ae5
pig1 <redsky21@lgcns.com>
2022-01-15 21:14:26 +0900

    fix: VO 경로수정

--- src/main/java/com/chs/boot/gerp/core/generate/model/TepGenServiceMethodInfoConditionVO.java
+++ src/main/java/com/chs/boot/gerp/core/generate/model/TepGenServiceMethodInfoConditionVO.java
@@ -7,4 +7,16 @@ import java.util.Date;
 import java.time.LocalDateTime;
 @Data
 public class TepGenServiceMethodInfoConditionVO {
+    private Long methodSeq;
+    private Long packageNo;
+    private String servicePackageName;
+    private String serviceClassName;
+    private String methodAnnotationName;
+    private String methodAccessor;
+    private String methodReturnClassName;
+    private String methodName;
+    private String methodParamClassName;
+    private String methodParamInstantName;
+    private String methodContents;
+    private String tableName;
 }
